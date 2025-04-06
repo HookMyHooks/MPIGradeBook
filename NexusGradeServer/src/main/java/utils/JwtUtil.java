ba3461fb41579d164f533d7f08e990d74011cdd5
Nedelcu Liviu Gabriel <147793169+HookMyHooks@users.noreply.github.com>
2025-04-06 08:34:58 +0300

    KEY-30 Updated dependecies and JwtUtil with logic

--- NexusGradeServer/src/main/java/utils/JwtUtil.java
+++ NexusGradeServer/src/main/java/utils/JwtUtil.java
@@ -1,4 +1,36 @@
 package utils;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
 public class JwtUtil {
+    private static final String SECRET_KEY = "your-256-bit-secret-which-is-at-least-32-bytes!";
+    private static final SecretKey key = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+
+    public static String generateToken(String username) {
+        return Jwts.builder()
+                .subject(username)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60)) // 1 hour
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+
+    public static String extractUsername(String token) {
+        return extractAllClaims(token).getSubject();
+    }
+
+    public static boolean isTokenValid(String token) {
+        return !extractAllClaims(token).getExpiration().before(new Date());
+    }
+
+    private static Claims extractAllClaims(String token) {
+        return Jwts.parser().setSigningKey(SECRET_KEY).build().parseClaimsJws(token).getBody();
+    }
 }
