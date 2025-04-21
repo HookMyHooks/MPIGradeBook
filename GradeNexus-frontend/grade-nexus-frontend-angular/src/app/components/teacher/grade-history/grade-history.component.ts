4f8148113aeebd53aa2a51a059005053cda40596
Beatrice Moraru <morarubeatrice77@yahoo.com>
2025-04-21 10:50:36 +0300

    [KEY-93] Replace hardcoded data for add grade and grade history

--- GradeNexus-frontend/grade-nexus-frontend-angular/src/app/components/teacher/grade-history/grade-history.component.ts
+++ GradeNexus-frontend/grade-nexus-frontend-angular/src/app/components/teacher/grade-history/grade-history.component.ts
@@ -1,16 +1,55 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { forkJoin } from 'rxjs';
+import { Grade } from '../../../dtos/grade';
+import { Student } from '../../../dtos/student';
+import { Subject } from '../../../dtos/subject';
+import { GradeService } from '../../../services/grade.service';
+import { StudentService } from '../../../services/student.service';
+import { SubjectService } from '../../../services/subject.service';
 
 @Component({
   standalone: true,
   selector: 'app-grade-history',
   templateUrl: './grade-history.component.html',
   styleUrls: ['./grade-history.component.scss'],
-  imports: [CommonModule]
+  imports: [CommonModule],
 })
-export class GradeHistoryComponent {
-  history: { student: string; subject: string; value: number; date: Date }[] = [
-    { student: 'Bianca Nechita', subject: 'Matematică', value: 9, date: new Date('2025-03-20') },
-    { student: 'Bianca Nechita', subject: 'Matematică', value: 8, date: new Date('2025-03-18') },
-  ];
+export class GradeHistoryComponent implements OnInit {
+  history: { student: string; subject: string; value: number; date: Date }[] =
+    [];
+  grades: Grade[] = [];
+  subjects: Subject[] = [];
+  students: Student[] = [];
+
+  constructor(
+    private readonly gradeService: GradeService,
+    private readonly studentService: StudentService,
+    private readonly subjectService: SubjectService
+  ) {}
+
+  ngOnInit(): void {
+    forkJoin({
+      grades: this.gradeService.getAllGrades(),
+      students: this.studentService.getAllStudents(),
+      subjects: this.subjectService.getAllSubjects(),
+    }).subscribe({
+      next: ({ grades, students, subjects }) => {
+        this.grades = grades;
+        this.students = students;
+        this.subjects = subjects;
+
+        this.history = grades.map((g) => {
+          const stud = students.find((s) => s.id === g.studentId);
+          const subj = subjects.find((s) => s.id === g.subjectId);
+          return {
+            student: stud ? `${stud.firstName} ${stud.lastName}` : 'Error',
+            subject: subj ? subj.name : 'N/A',
+            value: g.value,
+            date: new Date(g.date!),
+          };
+        });
+      },
+    });
+  }
 }
